a5b068905988395539b1ecb8136e2918a3f4328e
Marco Massarelli <60667061+ceoloide@users.noreply.github.com>
2025-09-04 12:59:24 -0400

    Fix `create_docker_image.sh` to look for `Dockerfile`

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,10 +1,10 @@
 ERGOGEN_VERSION=4.1.0
 FREEROUTING_VERSION=2.1.0
-if [ -d node_modules ]; then
+if [ -f Dockerfile ]; then
   docker build . -t ceoloide/ergogen-freerouting:${ERGOGEN_VERSION}_${FREEROUTING_VERSION}
   docker push ceoloide/ergogen-freerouting:${ERGOGEN_VERSION}_${FREEROUTING_VERSION}
   docker tag ceoloide/ergogen-freerouting:${ERGOGEN_VERSION}_${FREEROUTING_VERSION} ceoloide/ergogen-freerouting:latest
   docker push ceoloide/ergogen-freerouting:latest
 else
-  echo "Please run 'npm install' first."
+  echo "Dockerfile not found in the folder."
 fi
